baab045bb6578efa42036cfffde9e11b56debb28
adelalqaabi <adelalqaapi1998@gmail.com>
2025-01-12 00:15:39 +0300

    added new

--- support-form/src/stores/instance.js
+++ support-form/src/stores/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-//const baseURL = "https://www.cotankw.com/support"; //Prod
-const baseURL = "http://localhost:8000/support"; //Dev
+const baseURL = "https://www.cotankw.com/support"; //Prod
+//const baseURL = "http://localhost:8000/support"; //Dev
 
 const instance = axios.create({
   baseURL: baseURL,
